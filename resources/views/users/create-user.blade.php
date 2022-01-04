05a844f5b5d9145ec9c51e399bf899d8b2ca54e8
farjana001 <nipa.farzana87@gmail.com>
2022-01-04 17:18:01 +0600

    working on user module

---
+++ resources/views/users/create-user.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main-layout')
+
+@section('main_content')
+<h3>Create new user</h3>
+
+    <!-- DataTbales Example -->
+    <div class="card shadow mb-4 p-5 w-75">
+        <form action="{{ route('users.store') }}" enctype="multipart/form-data" method="POST">
+            @csrf
+            <div class="form-group">
+                <label style="color: black;" for="name">User Name:</label>
+                <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}" placeholder="Enter full name">
+                @error('name')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label style="color: black;" for="name">Email:</label>
+                <input type="text" id="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="Enter email address">
+            </div>
+            <div class="form-group">
+                <label style="color: black;" for="phone">Phone:</label>
+                <input type="text" id="phone" name="phone" class="form-control" value="{{ old('email') }}" placeholder="Enter phone number">
+            </div>
+
+            <div class="form-group">
+                <label style="color: black;" for="name">Address:</label>
+                <textarea type="text" id="address" name="address" class="form-control" value="{{ old('address') }}" rows="6" placeholder="Enter address"></textarea>
+            </div>
+
+
+            <div class="text-right pt-3">
+                <a href="{{ route('index.user.group') }}" class="btn btn-secondary px-5">Go Back</a>
+                <button type="submit" class="btn btn-success px-5">Create</button>
+            </div>
+        </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,16 @@ Route::get('/', function () {
     return view('layouts.main-layout');
 });
 
-// Route::get('users', function () {
-//     return view('users.users');
-// });
 
-Route::get('users', [UsersController::class, 'index']);
+
 Route::get('groups', [UserGroupsController::class, 'index'])->name('index.user.group');
 Route::get('create-group', [UserGroupsController::class, 'create'])->name('create.user.group');
 Route::post('create-group', [UserGroupsController::class, 'store'])->name('store.user.group');
 Route::get('create-group/edit/{id}', [UserGroupsController::class, 'edit'])->name('edit.user.group');
 Route::post('create-group/update/{id}', [UserGroupsController::class, 'update'])->name('update.user.group');
 Route::get('create-group/delete/{id}', [UserGroupsController::class, 'destroy'])->name('destroy.user.group');
+
+Route::resource('users', UsersController::class);
 
 
 
